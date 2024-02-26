d503f49b43296398bcbbd3b3d6b689c28976258c
imcamaro <ysc1993@yeah.net>
2024-02-26 16:23:27 +0800

    nothing

--- src/main/java/andy/AndyApplication.java
+++ src/main/java/andy/AndyApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AndyApplication {
     public static void main(String[] args){
+
         SpringApplication.run(AndyApplication.class, args);
 
         log.info("###############################");
