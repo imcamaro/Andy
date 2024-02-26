ffd0ee51dece2c87a0108167870a0d85343a1e37
imcamaro <ysc1993@yeah.net>
2024-02-26 16:19:41 +0800

    first commit

--- src/main/java/andy/AndyApplication.java
+++ src/main/java/andy/AndyApplication.java
@@ -1,11 +1,20 @@
 package andy;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class AndyApplication {
     public static void main(String[] args){
         SpringApplication.run(AndyApplication.class, args);
+
+        log.info("###############################");
+        log.info("#                             #");
+        log.info("#   Andy Start Successfully   #");
+        log.info("#                             #");
+        log.info("###############################");
+
     }
 }
