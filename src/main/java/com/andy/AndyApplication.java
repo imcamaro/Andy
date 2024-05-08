9700341d0cfaa31309c07e78f91216a8827ae721
imcamaro <ysc1993@yeah.net>
2024-05-08 11:07:57 +0800

    sync the code with master

--- src/main/java/com/andy/AndyApplication.java
+++ src/main/java/com/andy/AndyApplication.java
@@ -7,5 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AndyApplication {
     public static void main(String[] args){
         SpringApplication.run(AndyApplication.class, args);
+
+
     }
 }
