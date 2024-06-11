b0a43f1ffecb8bc0860386ab18cc97e2089c3381
Andy <ysc1993@yeah.net>
2024-06-12 07:33:52 +0800

    test commit new code

--- src/main/java/com/andy/AndyApplication.java
+++ src/main/java/com/andy/AndyApplication.java
@@ -1,11 +1,18 @@
 package com.andy;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class AndyApplication {
     public static void main(String[] args){
         SpringApplication.run(AndyApplication.class, args);
+        log.info("************************************");
+        log.info("*                                  *");
+        log.info("*  Application Start Successfully  *");
+        log.info("*                                  *");
+        log.info("************************************");
     }
 }
